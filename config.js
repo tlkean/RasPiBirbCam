39af24e66fad968a877ede8166278380ad14d9d4
prichardsondev <prichardsondev@gmail.com>
2022-05-12 10:56:03 -0400

    added config file

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    EXPRESS_PORT:"3000",
+    CLIENT:"PRICHARDSON",
+    MACHINE_ID:"01",
+    TABLE_NAME:"yourawstablename",
+    S3_BUCKET:"yourawsbucketname",
+    PROFILE:"default"
+}
